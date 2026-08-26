4bcd18bf84635cb283b401f82ee79a3c777a114c
Navid <navidabedin79@gmail.com>
2026-08-26 15:12:47 +0330

    style some  bug fixes & a constant withount change calculation added though a smart one commented

--- Projects/Onlineshop-coffee/js/modules/pricing.js
+++ Projects/Onlineshop-coffee/js/modules/pricing.js
@@ -7,8 +7,15 @@ export function calculatePrice({ arabicaVariant, robustaVariant, blendRatio, wei
   const total = perKg * weightKg * quantity;    
   
   return total;
-
 }
 
+/* const remainder = total % 10000;
 
+  const result = remainder === 0
+    ? total
+    : remainder >= 9000
+      ? Math.ceil(total / 10000) * 10000
+      : Math.floor(total / 10000) * 10000;
+
+  return result; */
 
